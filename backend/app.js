b2cddbb8e75ec957c122897bc41bcddce1e0bdc3
Eliya <eliyanoah70@gmail.com>
2022-12-21 00:37:19 +0200

    Added middleware to handle unsupported routes

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const HttpError = require("./models/http-error");
 
 const placesRoutes = require('./routes/places-routes');
 
@@ -7,6 +8,10 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use("/api/places", placesRoutes);
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+})
 app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
